d5e30d0335664a0d214cf92b1df5b49ff7f6d2b0
PSouza-10 <paulosouza300272@gmail.com>
2020-10-31 17:34:22 -0300

    removed warnings

--- src/components/CharacterWheel/index.js
+++ src/components/CharacterWheel/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { Container, ItemRow, ClearIcon } from './styles'
 
 import { ItemsContext } from '../../ItemsContext'
